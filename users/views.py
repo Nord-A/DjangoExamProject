8f482e7173ac7e5757f8800df5d64c87bd9c0f3f
Luis Lutzen <luis.lutzen@gmail.com>
2018-12-02 12:51:57 +0100

    Changed register method in views, to post

--- users/views.py
+++ users/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        # Vi mangler at skrive en validering her!
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
