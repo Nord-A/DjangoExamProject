5442e7776a37301431f5a16cab9bbd03bab41acc
Luis Lutzen <luis.lutzen@gmail.com>
2018-12-03 13:05:45 +0100

    Changes made to users.views.register form validation

--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        # Vi mangler at skrive en validering her!
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('forum-index')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
