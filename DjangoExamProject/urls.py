8514030c1f936cf61076d6fb8fd18d35f7ef7ed3
Nsvendsen <nilas@me.com>
2018-12-04 13:47:55 +0100

    Login added

--- DjangoExamProject/urls.py
+++ DjangoExamProject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls')),
     path('', include('forum.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', user_views.register, name="register")
 ]
