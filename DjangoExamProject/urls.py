635152be90edf17ab249e025fcd2fb11ee7caacc
Nsvendsen <nilas@me.com>
2018-12-05 10:49:19 +0100

    logout added

--- DjangoExamProject/urls.py
+++ DjangoExamProject/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('forum.urls')),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', user_views.register, name="register")
 ]
 
