1dacd716584821af31a2550077279a9db40f4617
Nsvendsen <nilas@me.com>
2018-12-06 12:05:41 +0100

    User test added

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .forms import UserRegisterForm
 
-# Create your tests here.
+
+# class UserRegisterForm(UserCreationForm):
+#     email = forms.EmailField()
+#
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
+
+# class SetupClass(TestCase):
+#
+#     def setUp(self):
+#         self.user = UserRegisterForm.objects.create(username = "Nilas", email = "nil@as.dk", password1 = "asdqwe33",
+#                                                     password2 = "asdqwe33")
+
+class UserRegister_Form_Test(TestCase):
+    def test_userregisterform_valid(self):
+        form = UserRegisterForm(data={'username':"Nilas", 'email':"nil@as.dk", 'password1':"asdqwe33",'password2':"asdqwe33"})
+        self.assertTrue(form.is_valid())
+
+
+    def test_userregisterform_invalid(self):
+        form = UserRegisterForm(data={'username':"asdfg", 'email':"nilas.com", 'password1':"asdqwe33",'password2':"asdqwe33"})
+        self.assertFalse(form.is_valid())
