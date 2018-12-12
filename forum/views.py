4b25b1c2c1d90d5802ca3b01bdf9a6a7d9d78326
JakobBB235 <33541242+JakobBB235@users.noreply.github.com>
2018-12-12 12:53:46 +0100

    Thread css updated, comments can now be rated

--- forum/views.py
+++ forum/views.py
@@ -74,7 +74,7 @@ def edit_thread(request, forum_thread_id):
 def view_thread(request, forum_thread_id):
     #Get currentuser here instead and if current_user.is_authenticated: to shorten code.
     if request.method == 'POST':
-        current_user = request.user  # Get current logged in user
+        current_user = request.user  # Get current logged in user.PERHAPS DO THIS IN THE START, BECAUSE IT IS USED A LOT
         if current_user.is_authenticated:
             thread = get_object_or_404(ForumThread, pk=forum_thread_id)  # Get thread from DB
 
@@ -99,22 +99,64 @@ def view_thread(request, forum_thread_id):
                     the_rating.save()
                 else:
                     old_rating = find_rating[0]  # find_rating is a list?? with only one item, so index 0 is that item.
+                    if old_rating.thumps_up != the_rating.thumps_up: #NEW prevents unnecessary save()
+                        old_rating.thumps_up = the_rating.thumps_up
+                        old_rating.save()
+
+            #Make elif here if ratings on comments is implemented?
+            elif 'comment_rating_like' in request.POST or 'comment_rating_dislike' in request.POST:
+                the_rating = Rating()
+                if 'comment_rating_like' in request.POST:
+                    the_rating.thumps_up = True
+                else:
+                    the_rating.thumps_up = False
+
+                # How to find out what comment was rated?
+                clicked_comment = Comment.objects.get(id=request.POST.__getitem__('comment_id'))  # Get comment from DB
+                find_rating = Rating.objects.filter(user=current_user, comment=clicked_comment)  # Get rating from DB
+                if len(find_rating) == 0:  # To ensure a user can only like or dislike a comment once.
+                    the_rating.user = current_user
+                    the_rating.comment = clicked_comment #Need to get this object
+                    the_rating.save()
+                else:
+                    old_rating = find_rating[0]  # find_rating is a list?? with only one item, so index 0 is that item.
                     old_rating.thumps_up = the_rating.thumps_up
                     old_rating.save()
 
-            #Make elif here if ratings on comments is implemented?
         else:  # If user is not logged in, redirect to login page
             return redirect('login')
     thread = get_object_or_404(ForumThread, pk=forum_thread_id)  # Get thread from DB
     thread.views_count += 1
     thread.save()
-    comments = Comment.objects.order_by("datetime_created").filter(thread=thread)  # Get comments from DB
 
+    comments = Comment.objects.order_by("datetime_created").filter(thread=thread)  # Get comments from DB
+    # To get the total amount of likes/dislikes for all comments on the current thread
+    for c in comments:
+        comment_ratings = Rating.objects.filter(comment=c)
+        comment_ratings_count_positive = sum(i.thumps_up == 1 for i in comment_ratings)
+        comment_ratings_count_negative = sum(i.thumps_up == 0 for i in comment_ratings)
+
+        # Attributes are added to the Comment model, no viewmodel is needed
+        c.comment_ratings_count_positive = comment_ratings_count_positive
+        c.comment_ratings_count_negative = comment_ratings_count_negative
+
+        # To show if user has clicked on like or dislike button (comments)
+        current_user = request.user
+        if current_user.is_authenticated:
+            find_rating = Rating.objects.filter(user=current_user, comment=c)  # length is 0 or 1. use get instead?
+            c.current_comment_rating = 'none'
+            if len(find_rating) != 0:
+                if find_rating[0].thumps_up:
+                    c.current_comment_rating = 'positive'
+                else:
+                    c.current_comment_rating = 'negative'
+
+    # To get the total amount of likes/dislikes for the current thread
     thread_ratings = Rating.objects.filter(thread=thread)  # Get ratings from DB
     thread_ratings_count_positive = sum(i.thumps_up == 1 for i in thread_ratings)
     thread_ratings_count_negative = sum(i.thumps_up == 0 for i in thread_ratings)
 
-    #This is to show if user has clicked on like or dislike button
+    # To show if user has clicked on like or dislike button (forumthreads)
     current_thread_rating = 'none'
     current_user = request.user  # Get current logged in user
     if current_user.is_authenticated:
@@ -125,9 +167,6 @@ def view_thread(request, forum_thread_id):
             else:
                 current_thread_rating = 'negative'
 
-    #Ratings on comments missing
-
-    # comment_ratings = Rating.objects.filter(comment=)
     # context = make the dictionary here and pass to render method instead?
     form = CommentForm()
     return render(request, 'forum/thread.html', {'thread': thread, 'comments': comments,
